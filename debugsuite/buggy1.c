b988491e2c590a36abc296d22df385e4e2bf7c42
Mahmoud <m.alhumsialrefaai@student.tudelft.nl>
2020-08-26 14:08:47 +0200

    Push loacl changes [To Be Revised]

--- debugsuite/buggy1.c
+++ debugsuite/buggy1.c
@@ -1,6 +1,5 @@
 #include <stdint.h>
 #include <assert.h>
-#include <stdio.h>
 
 /**
  * Increment integer.
@@ -10,28 +9,21 @@
  * @return The result of j incremented.
  */
 int16_t increment(int16_t j) {
-
-    //this does not work
-    //return j++;
-
-    return j+1; //FIX
+	return j++;
 }
 
 int main(int argc, char *argv[]) {
 
-    //this is breaking the code.
-    //const int n_increments = 100000; // Increment 100000 times.
+	const int n_increments = 100000; // Increment 100000 times.
 
-    const int n_increments = 32767; // FIX: an overflow occures after 32767 causing the number to become -32768
+	int16_t old_value = 0;
+	int16_t new_value = 0;
 
-    int16_t old_value = 0;
-    int16_t new_value = 0;
+	for (int i = 0; i < n_increments; ++i) {
+		new_value = increment(old_value);
+		assert(new_value == old_value + 1);
+		old_value = new_value;
+	}
 
-    for (int i = 0; i < n_increments; ++i) {
-        new_value = increment(old_value);
-        assert(new_value == old_value + 1);
-        old_value = new_value;
-    }
-
-    return 0;
+	return 0;
 }
--- debugsuite/buggy2.c
+++ debugsuite/buggy2.c
@@ -4,63 +4,27 @@
 
 int main(int argc, char *argv[]) {
 
-    printf("Calculator. \n");
-    printf("Usage: <operand1> <operation> <operand2>\n");
-    printf("Example: 2 + 5\n");
+	printf("Calculator. \n");
+	printf("Usage: <operand1> <operation> <operand2>\n");
+	printf("Example: 2 + 5\n");
 
-    //scanf does not like pointers
-//	int *operand1;
-//	int *operand2;
-//	char *operation;
+	int *operand1;
+	int *operand2;
+	char *operation;
+	scanf("%d %c %d", operand1, operation, operand2);
 
+	int result;
+	switch (*operation) {
+		case '+': result = *operand1 + *operand2;
+		case '-': result = *operand1 - *operand2;
+		case '*': result = *operand1 * *operand2;
+		case '/': result = *operand1 / *operand2;
+		default:
+			printf("Invalid operation\n");
+			break;
+	}
 
-//fix: dont use pointers and give scanf adress using &
-// better to init variables
-    int operand1;
-    int operand2;
-    char operation;
-    scanf("%d %c %d", &operand1, &operation, &operand2);
+	printf("> %d\n", result);
 
-
-//	scanf("%d %c %d", operand1, operation, operand2);
-
-
-    int result;
-
-    //fix: dont use pointers and break from the loop in the cases
-//	switch (*operation) {
-//		case '+': result = *operand1 + *operand2;
-//		case '-': result = *operand1 - *operand2;
-//		case '*': result = *operand1 * *operand2;
-//		case '/': result = *operand1 / *operand2;
-//		default:
-//			printf("Invalid operation\n");
-//			break;
-//	}
-
-
-    switch (operation) {
-        case '+':
-            result = operand1 + operand2;
-            break;
-        case '-':
-            result = operand1 - operand2;
-            break;
-        case '*':
-            result = operand1 * operand2;
-            break;
-
-        case '/':
-            result = operand1 / operand2;
-            break;
-
-        default:
-            printf("Invalid operation\n");
-            break;
-    }
-
-
-    printf("> %d\n", result);
-
-    return 0;
+	return 0;
 }
